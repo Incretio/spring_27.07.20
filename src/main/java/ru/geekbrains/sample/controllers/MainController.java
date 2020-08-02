feaf15e1197d653180d7c228496bd55f0ccef577
oleg rodin <oleg.rodin@gridnine.com>
2020-08-02 22:00:12 +0300

    1. Разобраться с примером проекта на Spring MVC. 2. Сделать контроллер (он уже на половину готов в проекте), который выведет список студентов на html-страницу students. для этого на странице нужно будет создать таблицу и воспользоваться тегом <tr th:each="student, iterator : ${students}"> 3. сделать страницу profile.html и вывести там данные одного студента в зависимости от id

--- src/main/java/ru/geekbrains/sample/controllers/MainController.java
+++ src/main/java/ru/geekbrains/sample/controllers/MainController.java
@@ -1,24 +1,40 @@
 package ru.geekbrains.sample.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import ru.geekbrains.sample.dto.Student;
+import ru.geekbrains.sample.repository.StudentsRepository;
 
 @Controller
 public class MainController {
 
-   @GetMapping("/")
-   public String getIndexPage() {
+    private StudentsRepository studentsRepository;
+
+    public MainController(@Autowired StudentsRepository studentsRepository) {
+        this.studentsRepository = studentsRepository;
+    }
+
+    @GetMapping("/")
+    public String getIndexPage() {
       return "index";
    }
 
     @GetMapping("/students")
-    public String getStudentPage() {
-        //чтобы вывести список студентов нужно добавить бин StudentsRepository положить данные в Model
-        return "student";
+    public String getStudentPage(Model model) {
+        model.addAttribute("students", studentsRepository.findAll());
+        return "students";
+    }
+
+    @GetMapping("/profile/{id}")
+    public String getStudentPage(Model model, @PathVariable long id) {
+        model.addAttribute("student", studentsRepository.findById(id));
+        return "profile";
     }
 
     @PostMapping("/students")
